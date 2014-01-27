f19332b4214ca7d33d73a15a206c0d395da379b1
mclement45 <cmatthieu45@gmail.com>
2014-01-27 09:26:47 +0100

    ajout des exceptions 14 19

--- app/models/NombreDecimal.java
+++ app/models/NombreDecimal.java
@@ -13,6 +13,22 @@ public class NombreDecimal {
     }
 
     public String en_romain() {
-	return "";
+	String res="";
+	if (valeur==4) return "IV";
+	if (valeur==9) return "IX";
+	if (valeur==14) return "XIV";
+	if (valeur==19) return "XIX";
+
+	if(valeur>=10){
+		res+="X"; valeur-=10;
+	}
+	if(valeur >=5 && valeur <=8){
+		res+="V";valeur-=5;
+	}
+	while(valeur >=1 && valeur <= 3){
+		res+="I";
+		valeur--;
+	}
+	return res;
     }
 }
--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -30,11 +30,45 @@ import models.NombreRomain;
 public class ApplicationTest {
 
     void assert_en_romain(int d, String r) {
-	assertThat(new NombreDecimal(d).en_romain()).isEqualTo(r);
+		assertThat(new NombreDecimal(d).en_romain()).isEqualTo(r);
     }
 
     @Test
     public void romain_unites() {
-	assert_en_romain(0,"");
+		assert_en_romain(0,"");
+		assert_en_romain(1,"I");
+		assert_en_romain(2,"II");
+		assert_en_romain(3,"III");
+		assert_en_romain(4,"IV");
+		assert_en_romain(5,"V");
+		assert_en_romain(6,"VI");
+		assert_en_romain(7,"VII");
+		assert_en_romain(8,"VIII");
+		assert_en_romain(9,"IX");
     }
+    
+    /*@Test
+    public void romain_dizaines(){
+		assert_en_romain(10,"X");
+		assert_en_romain(20,"XX");
+		assert_en_romain(30,"XXX");
+		assert_en_romain(40,"XL");
+		assert_en_romain(50,"L");
+		assert_en_romain(60,"LX");
+		assert_en_romain(70,"LXX");
+		assert_en_romain(80,"LXXX");
+		assert_en_romain(90,"XC");
+	}
+	/*@Test
+    public void romain_centaines(){
+		assert_en_romain(100,"C");
+		assert_en_romain(200,"CC");
+		assert_en_romain(300,"CCC");
+		assert_en_romain(400,"CD");
+		assert_en_romain(500,"D");
+		assert_en_romain(600,"DC");
+		assert_en_romain(700,"DCC");
+		assert_en_romain(800,"DCCC");
+		assert_en_romain(900,"CM");
+	}*/
 }
